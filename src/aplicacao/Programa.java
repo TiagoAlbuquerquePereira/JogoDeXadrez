8c3033882312fe54115164b3382aaa6a04407896
TiagoAlbuquerquePereira <tiagoalbuquerquepereira@hotmail.com>
2020-08-20 20:48:42 -0300

    Projeto Inicial

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Posicao pos = new Posicao(1, 2);
+		System.out.println(pos);
+	}
+
+}
